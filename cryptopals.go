838ee06b6de3b13be4c8af5ec3639cb85f97381d
Michael Fitz-Payne <mfitz.payne@gmail.com>
2018-04-11 21:29:43 +1000

    Completed challenge 5.

--- cryptopals.go
+++ cryptopals.go
@@ -14,15 +14,21 @@ import (
 const (
 	charSet       = " !\"#$%&\\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~"
 	lowerAlphaSet = "abcdefghijklmnopqrstuvwxyz"
+	iceKey        = "ICE"
 )
 
 var (
-	challenge1a    = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-	challenge1test = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-	challenge2a    = "1c0111001f010100061a024b53535009181c"
-	challenge2b    = "686974207468652062756c6c277320657965"
-	challenge2test = "746865206b696420646f6e277420706c6179"
-	challenge3a    = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+	challenge1a     = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+	challenge1test  = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+	challenge2a     = "1c0111001f010100061a024b53535009181c"
+	challenge2b     = "686974207468652062756c6c277320657965"
+	challenge2test  = "746865206b696420646f6e277420706c6179"
+	challenge3a     = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+	challenge5a     = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+	challenge5atest = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+
+	challenge5b     = "I go crazy when I hear a cymbal"
+	challenge5btest = "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
 )
 
 // engFreq represents the frequency of letter usage in the english language.
@@ -78,6 +84,9 @@ func main() {
 
 	fmt.Println("Challenge 4")
 	challenge4()
+
+	fmt.Println("Challenge 5")
+	challenge5()
 }
 
 func exit(success bool) {
@@ -144,7 +153,18 @@ func challenge4() {
 	}
 
 	winner := scoreStrings(results)
-	fmt.Printf("%s\n", winner)
+	fmt.Printf("%s", winner)
+}
+
+func challenge5() {
+	encrypted := []byte{}
+	for i, b := range challenge5a {
+		enc := byte(b) ^ iceKey[i%3]
+		encrypted = append(encrypted, enc)
+	}
+
+	test(hex.EncodeToString(encrypted), challenge5atest)
+
 }
 
 // hexStringsValid takes an array of hex-encoded strings, and returns the decoded
